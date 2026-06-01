a6630827cf1407dce3fd1803f3e151d7e159ac72
Silvia Frigatto <silvia.frigatto@gmail.com>
2026-06-01 19:07:02 -0300

    Adding multiple narrative projects.

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
 def describe_project(title, author):
     return f"Project '{title}' was created by {author}"
 
-title_input = input("Enter project title: ")
-author_input = input("Enter author name: ")
+projects = []
+
+quantity_input = input("How many projects do you want to register? ")
+
+try:
+    quantity = int(quantity_input)
+    for i in range (quantity):
+        title_input = input("Enter project title: ")
+        author_input = input("Enter author name: ")
+except ValueError:
+    print("Not an integer number.")    
+
 
 if not title_input and not author_input:
     print("Error: 'Project title' AND 'author' cannot be empty.")
@@ -12,4 +22,4 @@ elif not author_input:
     print("Error: Author cannot be empty.")    
 else:
     project_info = describe_project(title_input, author_input)
-    print(project_info)
+    print(project_info)    
