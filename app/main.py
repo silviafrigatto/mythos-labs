20dd7f27c0efb551c95b6b711e390a095bc29321
Silvia Frigatto <silvia.frigatto@gmail.com>
2026-06-02 16:12:09 -0300

    Finished: add multiple projects

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def describe_project(title, author):
     return f"Project '{title}' was created by {author}"
 
-projects = []
+sentence = []
 
 quantity_input = input("How many projects do you want to register? ")
 
@@ -10,16 +10,18 @@ try:
     for i in range (quantity):
         title_input = input("Enter project title: ")
         author_input = input("Enter author name: ")
+        if not title_input and not author_input:
+            print("Error: 'Project title' AND 'author' cannot be empty.")
+        elif not title_input:
+            print("Error: Project title cannot be empty.")
+        elif not author_input:    
+            print("Error: Author cannot be empty.")  
+        else:
+            project_info = describe_project(title_input, author_input)
+            sentence.append(project_info)
+    print(f"Registered products:\n{'\n'.join(sentence)}") 
 except ValueError:
     print("Not an integer number.")    
 
 
-if not title_input and not author_input:
-    print("Error: 'Project title' AND 'author' cannot be empty.")
-elif not title_input:
-    print("Error: Project title cannot be empty.")
-elif not author_input:    
-    print("Error: Author cannot be empty.")    
-else:
-    project_info = describe_project(title_input, author_input)
-    print(project_info)    
+   
