64cf2219994bbbaab80b9970b3fdda5d9842ad21
silviafrigatto <silvia.frigatto@gmail.com>
2026-06-07 22:39:10 -0300

    Fixed validation

--- app/main.py
+++ app/main.py
@@ -2,26 +2,42 @@ def describe_project(title, author):
     return f"Project '{title}' was created by {author}"
 
 sentence = []
+run_app = True
 
-quantity_input = input("How many projects do you want to register? ")
-
-try:
-    quantity = int(quantity_input)
-    for i in range (quantity):
-        title_input = input("Enter project title: ")
-        author_input = input("Enter author name: ")
-        if not title_input and not author_input:
-            print("Error: 'Project title' AND 'author' cannot be empty.")
-        elif not title_input:
-            print("Error: Project title cannot be empty.")
-        elif not author_input:    
-            print("Error: Author cannot be empty.")  
-        else:
+while run_app:
+    quantity_input = input("How many projects do you want to register? ")
+    try:
+        quantity = int(quantity_input)
+        while quantity > 0:
+            title_input = input("Enter project title: ")
+            author_input = input("Enter author name: ")
+            if not title_input or not author_input:
+                print("Error: 'Project title' and/or 'author' cannot be empty.")
+                continue
             project_info = describe_project(title_input, author_input)
             sentence.append(project_info)
-    print(f"Registered products:\n{'\n'.join(sentence)}") 
-except ValueError:
-    print("Not an integer number.")    
+            print(sentence)
+            quantity -= 1
+        run_app = False
+    except ValueError:
+        print("Error: Not an integer number.")
 
+        
+                
+        #for i in range (quantity):
+        #    title_input = input("Enter project title: ")
+        #    author_input = input("Enter author name: ")
+        #    if not title_input or not author_input:
+        #        print("Error: 'Project title' and/or 'author' cannot be empty.")
+            #elif not title_input:
+            #    print("Error: Project title cannot be empty.")
+            #elif not author_input:    
+            #    print("Error: Author cannot be empty.")  
+        #    else:
+        #        project_info = describe_project(title_input, author_input)
+        #        sentence.append(project_info)
+        #print(f"Registered products:\n{'\n'.join(sentence)}") 
+        #print(sentence)
+            
 
-   
+        
