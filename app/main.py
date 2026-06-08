ad51c5a4fcef1129a12e42d2e54eae0431979a70
silviafrigatto <silvia.frigatto@gmail.com>
2026-06-08 10:15:53 -0300

    Implmented dictionary for title and author

--- app/main.py
+++ app/main.py
@@ -2,9 +2,10 @@ def describe_project(title, author):
     return f"Project '{title}' was created by {author}"
 
 sentence = []
-run_app = True
+projects = []
+app_running = True
 
-while run_app:
+while app_running:
     quantity_input = input("How many projects do you want to register? ")
     try:
         quantity = int(quantity_input)
@@ -14,30 +15,15 @@ while run_app:
             if not title_input or not author_input:
                 print("Error: 'Project title' and/or 'author' cannot be empty.")
                 continue
-            project_info = describe_project(title_input, author_input)
-            sentence.append(project_info)
-            print(sentence)
+            projects.append({"Title": title_input, "Author": author_input})
             quantity -= 1
-        run_app = False
+        print("\nRegistered Projects:\n")
+        for project in projects:
+            print(f"Title: {project['Title']}")
+            print(f"Author: {project['Author']}\n")
+        app_running = False
     except ValueError:
         print("Error: Not an integer number.")
-
-        
-                
-        #for i in range (quantity):
-        #    title_input = input("Enter project title: ")
-        #    author_input = input("Enter author name: ")
-        #    if not title_input or not author_input:
-        #        print("Error: 'Project title' and/or 'author' cannot be empty.")
-            #elif not title_input:
-            #    print("Error: Project title cannot be empty.")
-            #elif not author_input:    
-            #    print("Error: Author cannot be empty.")  
-        #    else:
-        #        project_info = describe_project(title_input, author_input)
-        #        sentence.append(project_info)
-        #print(f"Registered products:\n{'\n'.join(sentence)}") 
-        #print(sentence)
             
 
         
