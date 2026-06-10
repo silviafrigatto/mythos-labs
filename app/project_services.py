2faff7f7d6933192d9cc5f20b3c821dbbb8a39b3
silviafrigatto <silvia.frigatto@gmail.com>
2026-06-10 12:49:16 -0300

    Created function: find_project_by_title

--- app/project_services.py
+++ app/project_services.py
@@ -3,3 +3,6 @@ def create_project(title, author):
         "title": title,
         "author": author
     }
+
+def find_project_by_title(projects, title):
+   return next((project for project in projects if project.get('title') == title), None) 
