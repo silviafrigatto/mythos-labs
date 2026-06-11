35cacced341793351b7ff6d75a2dc92d5dab98d0
silviafrigatto <silvia.frigatto@gmail.com>
2026-06-11 19:31:47 -0300

    Finished first test

--- tests/test_project_services.py
+++ tests/test_project_services.py
@@ -1,2 +1,10 @@
 from app.project_services import create_project
 
+def test_create_project():
+    project = create_project(
+        "Echoes of Ithaca",
+        "Helena"
+    )
+
+    assert project['title'] == "Echoes of Ithaca"
+    assert project['author'] == "Helena"
