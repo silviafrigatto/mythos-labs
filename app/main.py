d5f4bc9026acdb1b86efea1884cbacaa7c8a24fe
silviafrigatto <silvia.frigatto@gmail.com>
2026-06-11 10:53:40 -0300

    Creating a menu for the app

--- app/main.py
+++ app/main.py
@@ -4,7 +4,20 @@ projects = []
 app_running = True
 
 while app_running:
+
+    print("=== Mythos Labs ===\n")
+    print("1 - Register project")
+    print("2 - Search project")
+    print("3 - List project")
+    print("4 - Exit\n")
+    choice = input("Choose an action: ")
+
+    
+
+
+    #REGISTER A PROJECT
     quantity_input = input("How many projects do you want to register? ")
+    
     try:
         quantity = int(quantity_input)
         while quantity > 0:
@@ -16,14 +29,23 @@ while app_running:
             project = create_project(title_input, author_input)
             projects.append(project)
             quantity -= 1
+        
+        #LIST ALL PROJECTS
         print("\nRegistered Projects:\n")
         for project in projects:
             print(f"Title: {project['title']}")
             print(f"Author: {project['author']}\n")
-        title_find = input("Search project by title: ")
-        find = find_project_by_title(projects, title_find)
-        print(find)
+        
+        #SEARCH FOR A PROJECT
+        title_search = input("Search project by title: ")
+        title_search_result = find_project_by_title(projects, title_search)
+        if title_search_result == None:
+            print("Project not found.")
+        else:
+            print(title_search_result)
+        
         app_running = False
+    
     except ValueError:
         print("Error: Not an integer number.")
             
