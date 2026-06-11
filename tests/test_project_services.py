a51e418521b6afdd3cdb0c8d3b80b0772fdeeac6
silviafrigatto <silvia.frigatto@gmail.com>
2026-06-11 20:26:27 -0300

    Tested test_show_project_info

--- tests/test_project_services.py
+++ tests/test_project_services.py
@@ -1,4 +1,4 @@
-from app.project_services import create_project, find_project_by_title
+from app.project_services import create_project, find_project_by_title, show_project_info
 
 def test_create_project():
     project = create_project(
@@ -14,5 +14,14 @@ def test_find_project_by_title():
    project = find_project_by_title(projects, "Echoes of Ithaca")
    
    assert project['title'] == "Echoes of Ithaca"
+   assert project['author'] == "Helena"
 
+def test_show_project_info(capsys):
+    project = {"title": "Echoes of Ithaca", "author": "Helena"}
+    show_project_info(project)
+    captured = capsys.readouterr()
 
+    assert captured.out == (
+        "Title: Echoes of Ithaca\n"
+        "Author: Helena\n\n"
+    )
