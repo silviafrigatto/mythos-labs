08866a1ed27c1730b8b8fce6c3005fd74f42f5f9
silviafrigatto <silvia.frigatto@gmail.com>
2026-06-11 20:08:55 -0300

    Added another test function (test_find_project_by_title)

--- tests/test_project_services.py
+++ tests/test_project_services.py
@@ -1,4 +1,4 @@
-from app.project_services import create_project
+from app.project_services import create_project, find_project_by_title
 
 def test_create_project():
     project = create_project(
@@ -8,3 +8,11 @@ def test_create_project():
 
     assert project['title'] == "Echoes of Ithaca"
     assert project['author'] == "Helena"
+
+def test_find_project_by_title():
+   projects = [{"title": "Echoes of Ithaca", "author": "Helena"}]
+   project = find_project_by_title(projects, "Echoes of Ithaca")
+   
+   assert project['title'] == "Echoes of Ithaca"
+
+
