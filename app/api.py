26e2ff87c29eb39843ab53a2b55412a025d2c18d
silviafrigatto <silvia.frigatto@gmail.com>
2026-06-14 11:53:57 -0300

    Created "/projects" endpoint

--- app/api.py
+++ app/api.py
@@ -2,7 +2,21 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+projects = [
+    {
+        "title": "Echoes of Ithaca",
+        "author": "Helena"
+    },
+    {
+        "title": "The Last Oracle",
+        "author": "Marcus"
+    }
+]
 
 @app.get("/")
 def home():
     return {"message": "Welcome to Mythos Labs"}
+
+@app.get("/projects")
+def get_projects():
+    return projects
